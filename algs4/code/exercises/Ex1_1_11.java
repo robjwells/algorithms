00f4ea8249d44986c237a9f8b3c247d69e7bd0c0
Rob Wells <rob@robjwells.com>
2019-07-09 22:28:20 +0100

    algs4: Ex 1.1.11, organise imports

--- algs4/code/exercises/Ex1_1_11.java
+++ algs4/code/exercises/Ex1_1_11.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
-
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 class Ex1_1_11 {
     public static void main(String[] args) {
