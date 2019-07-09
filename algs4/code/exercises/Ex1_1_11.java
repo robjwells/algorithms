4faeaf4c676fcf05c65a76afb1cf668089a9361a
Rob Wells <rob@robjwells.com>
2019-07-09 22:16:15 +0100

    algs4: Ex 1.1.11

---
+++ algs4/code/exercises/Ex1_1_11.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.*;
+
+class Ex1_1_11 {
+    public static void main(String[] args) {
+        int limit = 10;
+        boolean[][] bools = new boolean[limit][limit];
+        for (int i = 0; i < limit * limit; i++) {
+            bools[i / limit][i % limit] = StdRandom.bernoulli();
+        }
+
+        // Print header line
+        StdOut.print("  ");
+        for (int i = 0; i < limit; i++) {
+            StdOut.print(i);
+        }
+        StdOut.println();
+
+        // Print matrix
+        String rowString;
+        for (int i = 0; i < limit; i++) {
+            boolean[] row = bools[i];
+            rowString = i + " ";
+            for (boolean item : row) {
+                rowString += item ? "*" : " ";
+            }
+            StdOut.println(rowString);
+        }
+    }
+}
