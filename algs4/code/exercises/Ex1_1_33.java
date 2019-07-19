44088a0d3cb461d84f3e19de7a062dcfd5be32f1
Rob Wells <rob@robjwells.com>
2019-07-19 14:45:22 +0100

    algs4: CP 1.1.33: Implement transpose and mult

--- algs4/code/exercises/Ex1_1_33.java
+++ algs4/code/exercises/Ex1_1_33.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.StdOut;
 
 class Ex1_1_33 {
@@ -7,5 +9,23 @@ class Ex1_1_33 {
             new double[] {4, -2, -1}
         );
         StdOut.printf("Dot result, expected 3.0, got %s\n", dotResult);
+
+        double[][] a = new double[][] {new double[] {1, 2, 3}, new double[] {4, 5, 6}};
+        StdOut.println("Matrix `a`:");
+        for (double[] row : a) {
+            StdOut.println(Arrays.toString(row));
+        }
+
+        double[][] transposed = Matrix.transpose(a);
+        StdOut.println("Matrix `a` after transposition:");
+        for (double[] row : transposed) {
+            StdOut.println(Arrays.toString(row));
+        }
+
+        double[][] multResult = Matrix.mult(a, transposed);
+        StdOut.println("Matrix `a` multiplied by its transposition");
+        for (double[] row : multResult) {
+            StdOut.println(Arrays.toString(row));
+        }
     }
 }
--- algs4/code/exercises/Matrix.java
+++ algs4/code/exercises/Matrix.java
@@ -7,4 +7,29 @@ public class Matrix {
             .mapToDouble(index -> x[index] * y[index])
             .sum();
     }
+
+    public static double[][] transpose(double[][] a) {
+        int aRows = a.length;
+        int aCols = a[0].length;
+
+        double[][] resultMatrix = new double[aCols][aRows];
+        for (int rowIdx = 0; rowIdx < aRows; rowIdx++) {
+            for (int colIdx = 0; colIdx < aCols; colIdx++) {
+                resultMatrix[colIdx][rowIdx] = a[rowIdx][colIdx];
+            }
+        }
+        return resultMatrix;
+    }
+
+    public static double[][] mult(double[][] a, double[][] b) {
+        // Tranpose b to allow use of dot product (cols become rows)
+        double[][] transposedB = transpose(b);
+        double[][] result = new double[a.length][a.length];
+        for (int aRowIdx = 0; aRowIdx < a.length; aRowIdx++) {
+            for (int bColIdx = 0; bColIdx < transposedB.length; bColIdx++) {
+                result[aRowIdx][bColIdx] = dot(a[aRowIdx], transposedB[bColIdx]);
+            }
+        }
+        return result;
+    }
 }
