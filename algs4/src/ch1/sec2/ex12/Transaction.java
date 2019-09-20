feac2335a12fd7c40e0554b7919f549ca7d3c430
Rob Wells <rob@robjwells.com>
2019-09-20 06:35:24 +0100

    algs4: Ex 1.2.12: Implement Transaction

---
+++ algs4/src/ch1/sec2/ex12/Transaction.java
@@ -0,0 +1,59 @@
+package ch1.sec2.ex12;
+
+import edu.princeton.cs.algs4.Date;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Transaction {
+    private final String name;
+    private final Date date;
+    private final double amount;
+
+    public Transaction(String who, Date when, double amount) {
+        this.name = who;
+        this.date = when;
+        this.amount = amount;
+    }
+
+    public Transaction(String transaction) {
+        String[] parts = transaction.split("\\s+");
+        if (parts.length != 3) {
+            throw new IllegalArgumentException(
+                    "Transaction does not match format of 'Name mm/dd/yyyy amount'"
+            );
+        }
+
+        this.name = parts[0];
+        this.date = new Date(parts[1]); // Throws IllegalArgumentException if invalid
+        try {
+            this.amount = Double.parseDouble(parts[2]);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException(
+                    "Could not parse given amount in transaction as a double",
+                    nfe
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+        StdOut.println(
+                new Transaction("Marx 5/5/1818 18.83") // Check the Transaction round-trips
+        );
+    }
+
+    public String who() {
+        return name;
+    }
+
+    public Date when() {
+        return date;
+    }
+
+    public double amount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %.2f", name, date, amount);
+    }
+}
