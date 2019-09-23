4dfd253d65412bdc398c13d83f1cd91bdbaab6c9
Rob Wells <rob@robjwells.com>
2019-09-23 21:12:05 +0100

    algs4: Ex 1.3.2

---
+++ algs4/src/ch1/sec3/ex02/ex1_3_02.java
@@ -0,0 +1,13 @@
+package ch1.sec3.ex02;
+
+/**
+ * Give the output printed by <code>java Stack</code> for the input:
+ *
+ * <code>it was - the best - of times - - - it was - the - -</code>
+ * <p>
+ * was best times of the was the it
+ * (1 left on stack)
+ */
+public class ex1_3_02 {
+}
+
