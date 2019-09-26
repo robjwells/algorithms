887e1ef0122deb79950e0cbf58ac914cfea668f9
Rob Wells <rob@robjwells.com>
2019-09-26 08:10:56 +0100

    algs4: Ex 1.3.14: Implement array resizing

--- algs4/src/ch1/sec3/ex14/ResizingArrayQueueOfStrings.java
+++ algs4/src/ch1/sec3/ex14/ResizingArrayQueueOfStrings.java
@@ -8,28 +8,35 @@ import edu.princeton.cs.algs4.StdOut;
  * The backing array is used as a circular buffer.
  */
 public class ResizingArrayQueueOfStrings {
-    private String[] queue = new String[10];
+    private static final int MIN_SIZE = 4;
+
+    private String[] queue = new String[MIN_SIZE];
     private int head;
     private int tail;
     private int size;
 
     public static void main(String[] args) {
         ResizingArrayQueueOfStrings q = new ResizingArrayQueueOfStrings();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 8; i++) {
             q.enqueue(String.valueOf(i));
         }
-        for (int j = 10; j < 15; j++) {
-            q.dequeue();
-            q.enqueue(String.valueOf(j));
-        }
-        for (int k = 0; k < 10; k++) {
+
+        // Clear and then fill space to check circular buffer works as intended
+        q.dequeue(); // 0
+        q.dequeue(); // 1
+        q.enqueue(String.valueOf(9));
+        q.enqueue(String.valueOf(10));
+        q.enqueue(String.valueOf(11));
+
+        while (q.size() != 0) {
             StdOut.println(q.dequeue());
         }
     }
 
     public void enqueue(String item) {
+        // Resize if full
         if (size == queue.length) {
-            throw new IllegalStateException("Queue is full; could not enqueue.");
+            resize(2 * queue.length);
         }
         queue[tail] = item;
         tail = (tail + 1) % queue.length;
@@ -44,6 +51,34 @@ public class ResizingArrayQueueOfStrings {
         queue[head] = null;
         head = (head + 1) % queue.length;
         size--;
+
+        // Resize if too empty
+        if (size == queue.length / 4 && queue.length / 2 >= MIN_SIZE) {
+            resize(queue.length / 2);
+        }
+
         return value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void resize(int capacity) {
+        String[] temp = new String[capacity];
+        if (size == 0) {
+            // Don't attempt to copy as queue is empty
+        } else if (tail > head) {
+            // Tail after head, so just copy the occupied range
+            System.arraycopy(queue, head, temp, 0, size);
+        } else {
+            // Tail before or at head, but the queue is not empty, so copy head->end then
+            // start->tail into the new array so that it runs head->tail without wrapping
+            System.arraycopy(queue, head, temp, 0, queue.length - head);
+            System.arraycopy(queue, 0, temp, queue.length - head, tail);
+        }
+        queue = temp;
+        head = 0;
+        tail = size;
     }
 }
