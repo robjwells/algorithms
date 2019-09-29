73391745d7f2eaea0b164fc2c9a848926fd5e1d0
Rob Wells <rob@robjwells.com>
2019-09-30 00:47:29 +0100

    algs4: Ex 1.3.20

--- algs4/src/ch1/sec3/ex19/LinkedList.java
+++ algs4/src/ch1/sec3/ex19/LinkedList.java
@@ -9,6 +9,26 @@ public class LinkedList<T> implements Iterable<T> {
     private Node<T> first;
 
     public static void main(String[] args) {
+        testRemoveLast();
+        testDeleteKth();
+    }
+
+    static void testDeleteKth() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        for (int index = 4; index >= 0; index--) {
+            list.delete(index);
+            for (int i : list) {
+                StdOut.print(i + " ");
+            }
+            StdOut.println();
+        }
+    }
+
+    static void testRemoveLast() {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
         list.add(2);
@@ -23,6 +43,7 @@ public class LinkedList<T> implements Iterable<T> {
         for (int i : list) {
             StdOut.print(i + " ");
         }
+        StdOut.println();
     }
 
     /**
@@ -39,7 +60,7 @@ public class LinkedList<T> implements Iterable<T> {
 
     /**
      * Remove the last node in the list, if one exists.
-     *
+     * <p>
      * Ex 1.3.19: Give a code fragment that removes the last node in a linked list
      * whose first node is <tt>first</tt>.
      */
@@ -58,6 +79,28 @@ public class LinkedList<T> implements Iterable<T> {
         }
         // `current` is now the last node, so remove the reference to it
         previous.next = null;
+    }
+
+    /**
+     * Remove the <tt>k</tt>th element of the linked list, if it exists.
+     *
+     * @param k the position of the element to remove (zero-based)
+     */
+    void delete(int k) {
+        Node<T> previous = null;
+        Node<T> current = first;
+        while (current != null && k > 0) {
+            previous = current;
+            current = current.next;
+            k--;
+        }
+        if (current != null && k == 0) {
+            if (previous == null) {
+                first = current.next;
+            } else {
+                previous.next = current.next;
+            }
+        }
     }
 
     @Override
