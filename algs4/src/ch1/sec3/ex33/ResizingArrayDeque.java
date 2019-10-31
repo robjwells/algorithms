29598dd817b2360d3b064c325a067ba66e132977
Rob Wells <rob@robjwells.com>
2019-10-31 23:41:51 +0000

    algs4: Ex 1.3.33: Resizing array deque (working)
    Appears to work but I’m wouldn’t be happy to use this
    as-is without writing a load of unit tests.

--- algs4/src/ch1/sec3/ex33/ResizingArrayDeque.java
+++ algs4/src/ch1/sec3/ex33/ResizingArrayDeque.java
@@ -1,70 +1,30 @@
 package ch1.sec3.ex33;
 
-import edu.princeton.cs.algs4.ResizingArrayQueue;
-
 import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class ResizingArrayDeque<Item> implements Deque<Item> {
-    private Item[] data = (Item[]) new Object[2];
-    private int front = 0;
-    private int rear = 1;
+    private static final int MINIMUM_SIZE = 2;
+    private Item[] data = (Item[]) new Object[MINIMUM_SIZE];
+    private int front;
+    private int rear;
     private int size;
 
     public static void main(String[] args) {
-        ResizingArrayDeque<Integer> d = new ResizingArrayDeque<>();
-        System.out.println(d);
-        d.pushLeft(1);
-        System.out.println(d);
-        d.pushLeft(2);
-        System.out.println(d);
-        d.pushRight(3);
-        System.out.println(d);
-        d.pushRight(4);
-        d.popLeft();
-        System.out.println(d);
-        d.popLeft();
-        System.out.println(d);
-        d.popLeft();
-        System.out.println(d);
-        d.popLeft();
-        System.out.println(d);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    private void resize(int maxSize) {
-        Item[] original = data;
-        if (size == 0) {
-            data = (Item[]) new Object[maxSize];
-            front = 0;
-            rear = 0;
-        } else if (front < rear) {
-            Item[] occupiedPortion = Arrays.copyOfRange(data, front + 1, rear);
-            Item[] resized = Arrays.copyOf(occupiedPortion, maxSize);
-            data = resized;
-            front = maxSize - 1;
-            rear = occupiedPortion.length;
-        } else {
-            Item[] frontPortion = Arrays.copyOfRange(data, front + 1, data.length);
-            Item[] rearPortion = Arrays.copyOfRange(data, 0, rear);
-            Item[] resized = Arrays.copyOf(frontPortion, maxSize);
-            int targetIndex = frontPortion.length;
-            for (Item item: rearPortion) {
-                resized[targetIndex] = item;
-                targetIndex++;
+        ResizingArrayDeque<String> d = new ResizingArrayDeque<>();
+        List<String> instructions = List.of("L1", "R2", "L3", "R4", "L5");
+        for (String inst : instructions) {
+            if (inst.startsWith("L")) {
+                d.pushLeft(inst);
+            } else {
+                d.pushRight(inst);
             }
-            data = resized;
-            front = maxSize - 1;
-            rear = frontPortion.length + rearPortion.length;
+            System.out.println(d);
+        }
+        while (!d.isEmpty()) {
+            d.popLeft();
+            System.out.println(d);
         }
     }
 
@@ -79,29 +39,109 @@ public class ResizingArrayDeque<Item> implements Deque<Item> {
     }
 
     @Override
-    public void pushLeft(Item item) {
-        if (size == data.length) {
-            resize(size * 2);
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void resize(int maxSize) {
+        System.out.println(Arrays.toString(data));
+        Item[] resizedArray = (Item[]) new Object[maxSize];
+        int target = 0;
+
+        /*
+            Indexes where you find the end elements (those elements which would
+            be returned by popLeft() and popRight(), respectively).
+        */
+        int frontmostIndex = indexToRight(front);
+        int rearmostIndex = indexToLeft(rear);
+
+        /*
+            A straight-through run of elements:
+            [ABCD    ]
+            [  ABCD  ]
+            [    ABCD]
+         */
+        if (frontmostIndex <= rearmostIndex) {
+            for (int source = frontmostIndex; source <= rearmostIndex; source++) {
+                resizedArray[target] = data[source];
+                target++;
+            }
         }
+        /*
+            Elements wrap around the ends of the array:
+            [BCD    A]
+            [CD    AB]
+            [D    ABC]
+         */
+        else {
+            for (int source = frontmostIndex; source < data.length; source++) {
+                resizedArray[target] = data[source];
+                target++;
+            }
+            for (int source = 0; source <= rearmostIndex; source++) {
+                resizedArray[target] = data[source];
+                target++;
+            }
+        }
+        front = resizedArray.length - 1;
+        rear = target;
+        data = resizedArray;
+        System.out.println(Arrays.toString(data));
+    }
+
+    private void halveStorageIfNecessary() {
+        // Backing array kept at least a minimum size
+        if (data.length > MINIMUM_SIZE && size == data.length / 4) {
+            resize(data.length / 2);
+        }
+    }
+
+    private void doubleStorageIfNecessary() {
+        // Double storage when size is one less than capacity to avoid the
+        // tricky situation where the array is full and the front and rear
+        // indices have moved past each other.
+        if (size == data.length - 1) {
+            resize(data.length * 2);
+        }
+    }
+
+    private int moveIndex(int currentIndex, int amount) {
+        return Math.floorMod(currentIndex + amount, data.length);
+    }
+
+    private int indexToLeft(int currentIndex) {
+        return moveIndex(currentIndex, -1);
+    }
+
+    private int indexToRight(int currentIndex) {
+        return moveIndex(currentIndex, 1);
+    }
+
+    @Override
+    public void pushLeft(Item item) {
+        doubleStorageIfNecessary();
         data[front] = item;
         size++;
-        front--;
-        if (front == -1) {
-            front = data.length - 1;
+        if (front == rear) {
+            rear = indexToRight(rear);
         }
+        front = indexToLeft(front);
     }
 
     @Override
     public void pushRight(Item item) {
-        if (size == data.length) {
-            resize(size * 2);
-        }
+        doubleStorageIfNecessary();
         data[rear] = item;
         size++;
-        rear++;
-        if (rear == data.length) {
-            rear = 0;
+        if (front == rear) {
+            front = indexToLeft(front);
         }
+        rear = indexToRight(rear);
     }
 
     @Override
@@ -109,14 +149,8 @@ public class ResizingArrayDeque<Item> implements Deque<Item> {
         if (size == 0) {
             throw new NoSuchElementException("Pop from an empty deque.");
         }
-        size--;
-        front++;
-        if (front == data.length) {
-            front = 0;
-        }
-        Item toReturn = data[front];
-        data[front] = null;
-        return toReturn;
+        front = indexToRight(front);
+        return pop(front);
     }
 
     @Override
@@ -124,13 +158,24 @@ public class ResizingArrayDeque<Item> implements Deque<Item> {
         if (size == 0) {
             throw new NoSuchElementException("Pop from an empty deque.");
         }
+        rear = indexToLeft(rear);
+        return pop(rear);
+    }
+
+    /*
+        We can abstract the pop method because the resizing comes last,
+        once the item has been retrieved, and the updated indices
+        (updated in resize()) aren't needed to perform the pop itself.
+
+        Contrast this with pushing, where if the backing array must increase
+        in size, the resize must take place and the new indices be set before
+        an attempt at placing the item into the backing array.
+     */
+    private Item pop(int index) {
         size--;
-        rear--;
-        if (rear == -1) {
-            rear = data.length - 1;
-        }
-        Item toReturn = data[rear];
-        data[rear] = null;
+        Item toReturn = data[index];
+        data[index] = null;
+        halveStorageIfNecessary();
         return toReturn;
     }
 }
