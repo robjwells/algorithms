c5156b17c826fcda77c12b0ed43dddebeb315f9c
Rob Wells <rob@robjwells.com>
2019-11-02 11:21:21 +0000

    algs4: Ex 1.3.35: RandomQueue

--- algs4/src/ch1/sec3/ex35/RandomQueue.java
+++ algs4/src/ch1/sec3/ex35/RandomQueue.java
@@ -1,8 +1,10 @@
 package ch1.sec3.ex35;
 
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class RandomQueue<Item> {
@@ -10,6 +12,20 @@ public class RandomQueue<Item> {
     @SuppressWarnings("unchecked")
     private Item[] data = (Item[]) new Object[10];
     private int size;
+
+
+    public static void main(String[] args) {
+        RandomQueue<Integer> q = new RandomQueue<>();
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        for (int i = 0; i < 10; i++) {
+            StdOut.print(i + ": ");
+            nums.forEach(q::enqueue);
+            while (!q.isEmpty()) {
+                StdOut.print(q.dequeue());
+            }
+            StdOut.println();
+        }
+    }
 
     public boolean isEmpty() {
         return size == 0;
